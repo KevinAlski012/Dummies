d18711188909706d804e8542ddc06bc455b98745
Gian-Abidano <80931223+Gian-Abidano@users.noreply.github.com>
2022-11-28 20:47:45 +0800

    Init branch
    Simple Dialogue Implementation

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] DialougeInteract introDialougeInteract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        introDialougeInteract.StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
